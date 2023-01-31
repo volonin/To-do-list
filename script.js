c411eed8dfee14b0b384e90bb9ac252fadb3ccdb
volonin <volonin.dmitry@gmail.com>
2023-01-31 22:28:07 +0200

    adding a due date

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 let completed = false;
 let array = [];
+let arrayText = [];
 let box = document.querySelector("main");
 let addTask = document.querySelector(".add");
-let input = document.querySelector("#add-task");
+let input = document.querySelector(".add-task");
 let footer = document.querySelector("footer");
-let x;
-
+let remuving;
+let x = 0;
+let textDate;
+let day = document.querySelector("#day");
+let month = document.querySelector("#month");
+let dayToday = new Date();
+let dayNow = dayToday.getDate();
+day.value = `${dayToday.getDate()}`;
+month.value = `0${dayToday.getMonth() + 1}`;
 document.addEventListener("keydown", function (e){
-    if (e.code == "Enter"){
-        addContent(input.value);
-        input.value = "";
+    if (e.code == "Enter") {
+        if (input.value === "") {
+            alert("Введите Task");
+        }else{
+            addContent(input.value);
+            input.value = "";
+        }
     }
 })
 
@@ -17,9 +29,16 @@ addTask.addEventListener("click", function (){
     if (input.value === ""){
         alert("Введите Task");
     }else{
+        x = 0;
         addContent(input.value);
         input.value = "";
     }
+})
+addTask.addEventListener("mouseover", function (){
+    addTask.firstElementChild.src = "Img/Group1.png";
+})
+addTask.addEventListener("mouseout", function (){
+    addTask.firstElementChild.src = "Img/Group.png";
 })
 function addContent (value){
     let div = document.createElement("div");
@@ -35,38 +54,54 @@ function addContent (value){
     text.innerHTML = value;
     text.classList = "text";
     div.append(text);
+    text = document.createElement("p");
+    text.classList.add("date-p");
+    switch (x){
+        case 0:text.innerHTML = `Выполнить до - ${day.value}.${month.value}`;
+        break;
+        case 1:text.innerHTML = textDate;
+        break;
+    }
+    box.lastElementChild.append(text);
+    arrayText.push(text);
     let buttun = document.createElement("button");
     buttun.classList.add("remuve");
     div.append(buttun);
+
     buttun.addEventListener("click", function (e){
-        console.log(e.target)
+        remuving = e.target.parentElement;
         e.target.parentElement.remove();
-    })
-    div.addEventListener("click", function (e){
-        e.target.style.backgroundColor = "#94ADCF";
-        if (completed){
-            e.target.style.backgroundColor = "";
-            completed = false;
-        }else{
-            completed = true;
-        }
+        let x = 0;
         for (i of array){
-            if(completed){
-                e.target.parentElement.classList.remove("notComplete");
-                e.target.parentElement.classList.add("complete");
-                x = e.target.nextElementSibling.innerHTML;
-                addContentInFooter();
-                console.log(e.target);
-                e.target.parentElement.remove();
+            if (i === remuving){
+                array.splice(x, 1);
                 break;
-            }else{
-                e.target.parentElement.classList.add("notComplete");
-                e.target.parentElement.classList.remove("complete");
             }
+            x++;
         }
+        text = arrayText[x];
+        text.remove();
+        arrayText.splice(x, 1);
+        console.log(text, arrayText);
     })
-}
-function addContentInFooter (){
+    img.addEventListener("click", function (e){
+        remuving = e.target.parentElement;
+        img.style.backgroundColor = "#94ADCF";
+        completed = true;
+        img.parentElement.classList.remove("notComplete");
+        img.parentElement.classList.add("complete");
+        x = img.nextElementSibling.innerHTML;
+        textDate = e.target.parentElement.lastElementChild.previousElementSibling.innerHTML;
+        addContentInFooter();
+        img.parentElement.remove();
+    })
+    if (dayNow > day.value && dayToday.getMonth() + 1 >= month.value){
+        div.style.backgroundColor = "#e55e5e";
+        // buttun.style.backgroundColor = "#ff2424";
+    }
+
+    }
+function addContentInFooter () {
     let div = document.createElement("div");
     div.classList = "completed";
     div.classList.add("complete");
@@ -79,18 +114,26 @@ function addContentInFooter (){
     text.innerHTML = `${x}`;
     text.classList = "text";
     div.append(text);
-    img.style.backgroundColor = "#94ADCF";
+    text = document.createElement("p");
+    text.classList.add("date-p");
+    text.innerHTML = `${textDate}`;
+    footer.lastElementChild.append(text);
+    arrayText.push(text);
+    img.style.backgroundColor = "#F7F5EB";
     let buttun = document.createElement("button");
     buttun.classList.add("remuve");
     div.append(buttun);
-    buttun.addEventListener("click", function (e){
-        console.log(e.target)
+
+
+    buttun.addEventListener("click", function (e) {
         e.target.parentElement.remove();
     })
-    div.addEventListener("click", function (e){
-        e.target.parentElement.classList.add("notComplete");
-        e.target.parentElement.classList.remove("complete");
-        addContent(e.target.nextElementSibling.innerHTML);
-        e.target.parentElement.remove();
+    img.addEventListener("click", function (e) {
+        img.parentElement.classList.add("notComplete");
+        img.parentElement.classList.remove("complete");
+        x = 1;
+        textDate = e.target.parentElement.lastElementChild.previousElementSibling.innerHTML;
+        addContent(img.nextElementSibling.innerHTML);
+        img.parentElement.remove();
     })
 }
